84899061c6b3ddbba798667e63930e253ec9ba34
FranZavalla <franzavalla85@gmail.com>
2026-02-13 13:43:21 -0300

    feat: removed express api

--- apps/api/src/config.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-
-export const config = {
-  NODE_ENV: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 3000
-};
--- apps/api/src/utils/helpers.ts
+++
@@ -1,5 +0,0 @@
-export const randomBytes = (length: number): Uint8Array => {
-  const bytes = new Uint8Array(length);
-  crypto.getRandomValues(bytes);
-  return bytes;
-};
