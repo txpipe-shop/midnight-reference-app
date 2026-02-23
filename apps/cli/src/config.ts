40fc3fe34dd44f275e5a62fc4008cbfb0d9e09ab
FranZavalla <franzavalla85@gmail.com>
2026-02-23 15:23:44 -0300

    feat: removed testcontainers and containers package

--- apps/cli/src/config.ts
+++ apps/cli/src/config.ts
@@ -1,6 +1,4 @@
-import type { TestContainers } from "@midnight-sentinel/containers";
 import { sentinelContractPrivateStateKey } from "@midnight-sentinel/contract";
-import "dotenv/config";
 import path from "node:path";
 
 export interface Config {
@@ -11,8 +9,6 @@ export interface Config {
   readonly indexerWS: string;
   readonly node: string;
   readonly proofServer: string;
-
-  updateConfigURLs(testContainers: TestContainers): void;
 }
 
 export const currentDir = path.resolve(new URL(import.meta.url).pathname, "..");
@@ -40,29 +36,4 @@ export class StandaloneConfig implements Config {
   indexerWS = "ws://127.0.0.1:8088/api/v3/graphql/ws";
   node = "http://127.0.0.1:9944";
   proofServer = "http://127.0.0.1:6300";
-
-  updateConfigURLs(testContainers: TestContainers): void {
-    this.indexer = testContainers.getContainerPort(
-      testContainers.getContainerName("indexer"),
-      this.indexer,
-    );
-    this.indexerWS = testContainers.getContainerPort(
-      testContainers.getContainerName("indexer"),
-      this.indexerWS,
-    );
-    this.node = testContainers.getContainerPort(
-      testContainers.getContainerName("node"),
-      this.node,
-    );
-    this.proofServer = testContainers.getContainerPort(
-      testContainers.getContainerName("proof-server"),
-      this.proofServer,
-    );
-  }
 }
-
-// TODO: validate env variables
-export const env = {
-  COMPOSE_DIR: process.env.COMPOSE_DIR!,
-  COMPOSE_FILE: process.env.COMPOSE_FILE!,
-};
--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,25 +1,16 @@
-import { TestContainers } from "@midnight-sentinel/containers";
 import { createLogger } from "@midnight-sentinel/logger";
 import { buildWallet, type WalletContext } from "@midnight-sentinel/wallet";
 import { stdin as input, stdout as output } from "node:process";
 import { createInterface } from "readline/promises";
 import { runCli } from "./cli/index.js";
-import { env, StandaloneConfig } from "./config.js";
+import { StandaloneConfig } from "./config.js";
 import { GENESIS_MINT_WALLET_SEED } from "./utils/constants.js";
 
 const config = new StandaloneConfig();
 const logger = createLogger(config.logDir);
-const testContainers = new TestContainers(
-  env.COMPOSE_DIR,
-  env.COMPOSE_FILE,
-  logger,
-);
 
 const main = async () => {
   console.log("Starting");
-  await testContainers.start();
-  console.log("Test containers began");
-  // config.updateConfigURLs(testContainers);
   const walletCtx: WalletContext = await buildWallet(
     config,
     GENESIS_MINT_WALLET_SEED,
@@ -32,6 +23,4 @@ const main = async () => {
   process.exit(0);
 };
 
-await main()
-  .catch(console.error)
-  .finally(testContainers.stop.bind(testContainers));
+await main().catch(console.error);
