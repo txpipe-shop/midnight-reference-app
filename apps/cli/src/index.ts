3d375f82db49f1c69a79c49985d01a33b30cc6f7
Nicolas Ludueña <luduena.nicolas.victorio@gmail.com>
2026-02-23 18:33:11 -0300

    chore: update seed phrase to use the undeployed wallet seedphrase

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -4,21 +4,14 @@ import { buildWallet, type WalletContext } from "@midnight-sentinel/wallet";
 import { stdin as input, stdout as output } from "node:process";
 import { createInterface } from "readline/promises";
 import { runCli } from "./cli/index.js";
-import { env, StandaloneConfig } from "./config.js";
+import { StandaloneConfig } from "./config.js";
 import { GENESIS_MINT_WALLET_SEED } from "./utils/constants.js";
 
 const config = new StandaloneConfig();
 const logger = createLogger(config.logDir);
-const testContainers = new TestContainers(
-  env.COMPOSE_DIR,
-  env.COMPOSE_FILE,
-  logger,
-);
 
 const main = async () => {
   console.log("Starting");
-  await testContainers.start();
-  console.log("Test containers began");
   // config.updateConfigURLs(testContainers);
   const walletCtx: WalletContext = await buildWallet(
     config,
@@ -33,5 +26,4 @@ const main = async () => {
 };
 
 await main()
-  .catch(console.error)
-  .finally(testContainers.stop.bind(testContainers));
+  .catch(console.error);
