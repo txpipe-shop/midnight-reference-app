8318c3ebadd9514295b50f7c023b3566ec48a8f8
Lola Aimar <lolaaimar14@gmail.com>
2026-03-27 17:34:37 -0300

    third times the charm

--- packages/contract/src/providers.ts
+++ packages/contract/src/providers.ts
@@ -4,11 +4,7 @@ import { levelPrivateStateProvider } from '@midnight-ntwrk/midnight-js-level-pri
 import { NodeZkConfigProvider } from '@midnight-ntwrk/midnight-js-node-zk-config-provider';
 import { type WalletContext, createWalletAndMidnightProvider } from '@midnight-sentinel/wallet';
 import path from 'node:path';
-import {
-  SentinelContractCircuitKeys,
-  SentinelContractProviders,
-  PrivateStateId,
-} from './types.js';
+import { SentinelContractCircuitKeys, SentinelContractProviders, PrivateStateId } from './types.js';
 
 // TODO: Maybe we can improve how these variables are defined
 const currentDir = path.resolve(new URL(import.meta.url).pathname, '..');
