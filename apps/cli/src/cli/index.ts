e2e2f70db9b262351a693976a155af24a1e299c9
Nicolas Ludueña <luduena.nicolas.victorio@gmail.com>
2026-04-20 15:33:32 -0300

    feat: redeem rewards

--- apps/cli/src/cli/index.ts
+++ apps/cli/src/cli/index.ts
@@ -28,7 +28,11 @@ async function handleCircuits(
         }
         return;
       case '2':
-        console.log('Not implemented.');
+        try {
+          await contract.redeemRewards();
+        } catch (e) {
+          console.log('Error redeeming rewards: ', e);
+        }
         return;
       case '3':
         console.log('Not implemented.');
